6a20851c78ec06512b4812d74ddf254beabdd4db
SebaKrk <s.sciuba@gmail.com>
2024-01-20 07:53:31 +0100

    Initial Commit

---
+++ MyNBPapp/MyNBPappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNBPappApp.swift
+//  MyNBPapp
+//
+//  Created by Sebastian Sciuba on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyNBPappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
