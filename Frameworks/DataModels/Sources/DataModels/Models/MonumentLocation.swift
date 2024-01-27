9e155385f6704999c7d32bca3662c46277018b43
SebaKrk <s.sciuba@gmail.com>
2024-01-27 16:47:08 +0100

    IOS-0005 `MonumentKit` IOS-0005B Wstępna Implementacja

--- Frameworks/DataModels/Sources/DataModels/Models/MonumentLocation.swift
+++ Frameworks/DataModels/Sources/DataModels/Models/MonumentLocation.swift
@@ -11,6 +11,8 @@ import CoreLocation
 /// `MonumentLocation` is a structure representing a specific monument location.
 public struct MonumentLocation: Identifiable, Hashable {
     
+    // MARK: - Properties
+    
     /// Unique identifier for the monument location, derived from its name.
     public var id: String { name }
     
@@ -25,5 +27,17 @@ public struct MonumentLocation: Identifiable, Hashable {
     
     /// Default distance for the camera view when the monument is displayed on a map. Default is 1000 meters.
     public var cameraDistance: Double = 1000
+    
+    // MARK: - Lifecycle
+    
+    public init(name: String,
+         location: CLLocation,
+         description: String,
+         cameraDistance: Double) {
+        self.name = name
+        self.location = location
+        self.description = description
+        self.cameraDistance = cameraDistance
+    }
     
 }
