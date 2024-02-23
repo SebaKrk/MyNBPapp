ef3fb752e9650fbb62101f2b6ed4c002a7cea2d5
SebaKrk <s.sciuba@gmail.com>
2024-02-23 12:19:16 +0100

    IOS-0010 `Swift OpenAPI Generator` IOS-0010B Wstępne testowanie api

--- ExchangeRateClient/ExchangeRateClient.swift
+++ ExchangeRateClient/ExchangeRateClient.swift
@@ -10,6 +10,25 @@ import OpenAPIRuntime
 import OpenAPIURLSession
 import HTTPTypes
 
+public struct ExchangeRatesResponse: Codable {
+    public let base: String?
+    public let date: String?
+    public let rates: [String: Double]?
+    public let timeLastUpdated: Int?
+
+    public init(base: String?, date: String?, rates: [String : Double]?, timeLastUpdated: Int?) {
+        self.base = base
+        self.date = date
+        self.rates = rates
+        self.timeLastUpdated = timeLastUpdated
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case base, date, rates
+        case timeLastUpdated = "time_last_updated"
+    }
+}
+
 public struct ExchangeRateClient {
     
     let client: Client
@@ -20,7 +39,8 @@ public struct ExchangeRateClient {
                              transport: transport)
     }
     
-    public func getData() async throws {
+    
+    public func getData() async throws  {
         let response = try await client.get_sol_latest_sol__lcub_base_currency_rcub_(path: .init(base_currency: "PLN"))
         
         switch response {
@@ -30,15 +50,20 @@ public struct ExchangeRateClient {
             case .json(let json):
                 dump(json)
             }
+            
         case .notFound(_):
             print("not found")
         case .undocumented(statusCode: let statusCode, _):
             print("Undocumented status code: \(statusCode)")
         }
     }
-    
 }
 
 //        get_sol_latest_sol__lcub_base_currency_rcub_()
 //        get_sol_latest_sol__lcub_base_currency_rcub_(path: )
 //        get_sol_latest_sol__lcub_base_currency_rcub_(path: ,headers:)
+
+//                let exchangeRates = try JSONDecoder().decode(ExchangeRatesResponse.self, from: json)
+//                let decoder = JSONDecoder()
+//                let exchangeRates = try decoder.decode(ExchangeRatesResponse.self, from: <#T##Data#>)
+//                return exchangeRates
