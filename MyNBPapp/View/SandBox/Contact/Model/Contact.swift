a56b8101e36c5c1b984875d6a6bcd252d7318f1b
SebaKrk <s.sciuba@gmail.com>
2024-06-03 23:06:55 +0200

    IOS-0023 TCA `Stack-based navigation` IOS-0023B SandBox - Favorites flow

--- MyNBPapp/View/SandBox/Contact/Model/Contact.swift
+++ MyNBPapp/View/SandBox/Contact/Model/Contact.swift
@@ -6,8 +6,23 @@
 //
 
 import Foundation
+import SwiftData
+
 
 struct Contact: Equatable, Identifiable {
   let id: UUID
   var name: String
+}
+
+@Model
+class ContactTest {
+    @Attribute(.unique) var id: UUID
+    var name: String
+    var phone: String
+    
+    init(id: UUID, name: String, phone: String) {
+        self.id = id
+        self.name = name
+        self.phone = phone
+    }
 }
