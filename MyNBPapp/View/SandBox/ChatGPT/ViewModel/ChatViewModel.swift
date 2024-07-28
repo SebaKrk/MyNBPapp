8ac77e2e8c81a08fe2369a01fb227d1fffec33a4
SebaKrk <s.sciuba@gmail.com>
2024-07-28 22:32:57 +0200

    IOS-0024 ChatGPT IOS-0024A SandBox - ChatGPT

--- MyNBPapp/View/SandBox/ChatGPT/ViewModel/ChatViewModel.swift
+++ MyNBPapp/View/SandBox/ChatGPT/ViewModel/ChatViewModel.swift
@@ -10,7 +10,7 @@ import Alamofire
 
 class ChatViewModel: ObservableObject {
     
-    @Published var messages: [Message] = []
+    @Published var messages: [Message] = [Message(id: UUID(), role: .system, content: "You are coding assistant. You will help me understand how to write only Swift code. You do not have enough information about other languages to give advice so avoid doing so at ALL times", creatAt: Date())]
     @Published var currentInput: String = ""
     
     private let openAIServices = OpenAIServices()
@@ -32,56 +32,4 @@ class ChatViewModel: ObservableObject {
             }
         }
     }
-}
-
-enum Constants {
-    static let openAIApiKey = ""
-}
-
-class OpenAIServices {
-    
-    private let endPointURL = "https://api.openai.com/v1/chat/completions"
-    
-    func sendMessage(messages: [Message]) async -> OpenAIChatResponse? {
-        let openAIMessages = messages.map { OpenAIChatMessage(role: $0.role, content: $0.content)}
-        
-        let body = OpenAIChatBody(model: "gpt-3.5-turbo", messages: openAIMessages)
-        let headers: HTTPHeaders = [
-            "Authorization" : "Bearer \(Constants.openAIApiKey)"
-        ]
-        
-        return try? await AF.request(endPointURL, method: .post, parameters: body, encoder: .json, headers: headers).serializingDecodable(OpenAIChatResponse.self).value
-    }
-    
-}
-
-struct Message: Decodable {
-    let id: UUID
-    let role: SenderRole
-    let content: String
-    let creatAt: Date
-}
-
-struct OpenAIChatBody: Encodable {
-    let model: String
-    let messages: [OpenAIChatMessage]
-}
-
-struct OpenAIChatMessage: Codable {
-    let role: SenderRole
-    let content: String
-}
-
-enum SenderRole: String, Codable {
-    case system
-    case user
-    case assistant
-}
-
-struct OpenAIChatResponse: Codable {
-    let choice: [OpenAIChatChoice]
-}
-
-struct OpenAIChatChoice: Codable {
-    let message: OpenAIChatMessage
 }
